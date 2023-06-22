b2a5a9783d99b484d0513132c3b5f9bd9e233768
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-06-22 16:44:10 +0530

    Book AND Author

--- src/main/java/com/example/Student_Library_Management_System/Repositories/StudentRepository.java
+++ src/main/java/com/example/Student_Library_Management_System/Repositories/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.Student_Library_Management_System.Repositories;
 
+
 import com.example.Student_Library_Management_System.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/com/example/Student_Library_Management_System/Services/StudentService.java
+++ src/main/java/com/example/Student_Library_Management_System/Services/StudentService.java
@@ -1,9 +1,10 @@
 package com.example.Student_Library_Management_System.Services;
 
 
-import com.example.Student_Library_Management_System.DTOs.StudentUpdateMobRequestDto;
+
 import com.example.Student_Library_Management_System.Enums.CardStatus;
 import com.example.Student_Library_Management_System.Models.Card;
+
 import com.example.Student_Library_Management_System.Models.Student;
 import com.example.Student_Library_Management_System.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +28,6 @@ public class StudentService {
         Card card = new Card();
         card.setCardStatus(CardStatus.ACTIVATED); //Card status is being set.
         card.setStudentVariableName(student); //Foreign key attribute
-        //Filling the value of the unidirectional part.
 
 
         //Lets go to the student
@@ -35,53 +35,14 @@ public class StudentService {
 
 
         //If there was a unidirectional mapping : we had to save both of them them
-        //studentRepo.save () and cardRepo.save()
+
         //But we are super advance and are using bidirectional : Child will automatically be saved.
 
         studentRepository.save(student);
-        //By cascading effect, child will automatically be saved (cardRepo will be saved)
+
+        //By cascading effect, child will automatically be saved.
 
         return "Student and Card added";
-    }
-
-
-    public String findNameByEmail(String email){
-
-        Student student = studentRepository.findByEmail(email);
-
-        return student.getName();
-    }
-
-
-    public String updateMobNo(StudentUpdateMobRequestDto studentReq){
-
-
-
-        //CONVERT THE DTO TO ENTITY : saved better
-
-
-
-        //First we will try to fetch originalData
-        Student originalStudent = studentRepository.findById(studentReq.getId()).get();
-
-        //We will keep the other properties as it is : and only change the required parameters
-
-        originalStudent.setMobNo(studentReq.getMobNo());
-
-
-        //Always entity object is being saved.
-        studentRepository.save(originalStudent);
-
-        return "Student has been updated successfully. ";
 
     }
-
 }
-
-/*
-    1. Existing Functions with no definition.
-
-    2. Existing function + with defining.
-
-    3. New Fu
- */
