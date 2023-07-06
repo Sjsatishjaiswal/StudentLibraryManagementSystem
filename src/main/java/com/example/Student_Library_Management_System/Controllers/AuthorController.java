3f1879fd5c0dff288cf8180e7f8d99c00bc2bf32
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-07-06 14:23:44 +0530

    Student Library Management System Projects

--- src/main/java/com/example/Student_Library_Management_System/Controllers/AuthorController.java
+++ src/main/java/com/example/Student_Library_Management_System/Controllers/AuthorController.java
@@ -1,12 +1,12 @@
 
+import com.example.Student_Library_Management_System.DTOs.AuthorEntryDto;
+import com.example.Student_Library_Management_System.DTOs.AuthorResponseDto;
 import com.example.Student_Library_Management_System.Models.Author;
 import com.example.Student_Library_Management_System.Services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
 @RestController
 @RequestMapping("author")
 public class AuthorController {
@@ -26,3 +26,37 @@ public class AuthorController {
     }
 
 }
+**/
+
+
+
+@RestController
+@RequestMapping("author")
+public class AuthorController {
+
+    //    / is optional : you want to write you can
+    // otherwise not required
+
+
+    @Autowired
+    AuthorService authorService;
+
+    @PostMapping("/add")
+    public String addAuthor(@RequestBody AuthorEntryDto authorEntryDto){
+        return authorService.createAuthor(authorEntryDto);
+    }
+
+    @GetMapping("/getAuthor")
+    public AuthorResponseDto getAuthor(@RequestParam("authorId")Integer authorId){
+        return authorService.getAuthor(authorId);
+    }
+
+
+
+
+
+
+
+
+}
+
--- src/main/java/com/example/Student_Library_Management_System/StudentLibraryManagementSystemApplication.java
+++ src/main/java/com/example/Student_Library_Management_System/StudentLibraryManagementSystemApplication.java
@@ -2,6 +2,10 @@ package com.example.Student_Library_Management_System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -10,5 +14,12 @@ public class StudentLibraryManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentLibraryManagementSystemApplication.class, args);
 	}
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				.apis(RequestHandlerSelectors.basePackage("com.example.Student_Library_Management_System")).build();
+//	}
+
+
 
 }
